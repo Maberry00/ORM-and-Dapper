88b506ede80851d35c4d8185b01e4c62529ceb73
Maberry00 <maberry30@gmail.com>
2024-02-14 09:26:49 -0600

    update

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection conn;
+
+        public DapperDepartmentRepository(IDbConnection conn)
+        {
+            this.conn = conn;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return this.conn.Query<Department>("SELECT * FROM departments");
+        }
+
+        public void InsertDepartment(string name) 
+        {
+            this.conn.Execute("INSERT INTO departments (Name) VALUES (@name)", new {name = name });
+        }
+    }
+}
--- ORM-Dapper/obj/Debug/net6.0/ORM-Dapper.AssemblyInfo.cs
+++ ORM-Dapper/obj/Debug/net6.0/ORM-Dapper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1cdb5708f131f9adf2f68177bbaad8d96821906")]
 [assembly: System.Reflection.AssemblyProductAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ORM-Dapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
